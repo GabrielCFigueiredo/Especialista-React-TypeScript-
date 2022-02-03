f78e76184f938ce3e5ac5f0ed383700077416be2
Gabriel <gabriel.devsistema@gmail.com>
2022-02-03 20:42:16 -0300

    add new feature

---
+++ alganews-sdk/dist/index.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function log() {
+    console.log('uhull eu sou ');
+}
+exports.default = log;
---
+++ alganews-sdk/src/index.ts
@@ -0,0 +1,11 @@
+export default function log(){
+    console.log('uhull eu sou ' );
+    
+}
+
+export function feature() {
+
+    console.log('feature');
+    
+    
+}
